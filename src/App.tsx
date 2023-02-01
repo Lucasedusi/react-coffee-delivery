fe22b84644ca6e44c456bc973e641fa995cfc752
lucasedusi <lucasmto.16@hotmail.com>
2023-02-01 16:54:41 -0300

    update file

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,7 @@
 import { useEffect, useState } from "react";
 
 function App() {
-	const [count, setCount] = useState(() => {
-		const savedItem = localStorage.getItem("dataKey");
-
-		if (savedItem) {
-			return JSON.parse(savedItem);
-		}
-	});
-
-	useEffect(() => {
-		localStorage.setItem("dataKey", JSON.stringify(count));
-	}, [count]);
-
-	function handlePlus() {
-		setCount(count + 1);
-	}
-
-	function handleMenos() {
-		setCount(count - 1);
-	}
-
-	return (
-		<div>
-			<button onClick={handlePlus}>aumentar</button>
-			<button onClick={handleMenos} disabled={count <= 0}>
-				Dimuir
-			</button>
-			<h1>{count}</h1>
-		</div>
-	);
+	<h1>Coffee Delivery</h1>;
 }
 
 export default App;
