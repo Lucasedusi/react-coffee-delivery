d61b53e314cc35bdba31889b2d12c405508b9a10
lucasedusi <lucasmto.16@hotmail.com>
2023-02-07 17:26:56 -0300

    feat: routes config

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,16 @@ import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/theme/default";
 import { GlobalStyle } from "./styles/global";
 
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Routes";
+
 function App() {
 	return (
 		<ThemeProvider theme={defaultTheme}>
-			<h1>Teste</h1>
 			<GlobalStyle />
+			<BrowserRouter>
+				<Router />
+			</BrowserRouter>
 		</ThemeProvider>
 	);
 }
