aeb3440dfa2b303298ee315803ad47e50f329df2
lucasedusi <lucasmto.16@hotmail.com>
2023-02-08 14:39:41 -0300

    feat: add header styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,29 @@
-import { HeaderContainer } from "./styles";
+import {
+	HeaderButtonCart,
+	HeaderButtonLocation,
+	HeaderButtonsContainer,
+	HeaderContainer,
+} from "./styles";
 
 import coffeeDeliveryLogo from "../../assets/coffee-delivery-logo.svg";
+import { MapPin, ShoppingCart } from "phosphor-react";
 
 export function Header() {
 	return (
 		<HeaderContainer>
-			<div>
+			<div className="container">
 				<img src={coffeeDeliveryLogo} />
+
+				<HeaderButtonsContainer>
+					<HeaderButtonLocation>
+						<MapPin size={20} weight="fill" />
+						Porangatu, GO
+					</HeaderButtonLocation>
+
+					<HeaderButtonCart>
+						<ShoppingCart size={20} weight="fill" />
+					</HeaderButtonCart>
+				</HeaderButtonsContainer>
 			</div>
 		</HeaderContainer>
 	);
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,43 @@
 import styled from "styled-components";
 
-export const HeaderContainer = styled.header``;
+export const HeaderContainer = styled.header`
+	width: 100%;
+	height: 6.5rem;
+	background: ${({ theme }) => theme.colors["base-background"]};
+	display: flex;
+	align-items: center;
+	justify-content: center;
+
+	> div {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
+`;
+
+export const HeaderButtonsContainer = styled.div`
+	display: flex;
+	align-items: center;
+	gap: 0.75rem;
+`;
+
+export const HeaderButtonBase = styled.button`
+	position: relative;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	gap: 4px;
+	min-width: 2.3rem;
+	height: 2.3rem;
+	border-radius: 6px;
+	border: none;
+	padding: 0 0.5rem;
+`;
+
+export const HeaderButtonLocation = styled(HeaderButtonBase)`
+	background: #f00;
+`;
+
+export const HeaderButtonCart = styled(HeaderButtonBase)`
+	background: #ff0;
+`;
